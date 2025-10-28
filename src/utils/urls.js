39826483fab6368967a71d297064dc7c424f9122
nygil8 <nygilbinoy83@gmail.com>
2025-10-28 12:11:22 +0530

    added backend url

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,5 +1,5 @@
 
-export const API_URL = "http://localhost:5000"
+export const API_URL = "https://irohub-backend-1.onrender.com"
 
 export const getAuthorized = (additionalHeaders = {}) => {
     const user = sessionStorage.getItem('metadataUser') || '{}'
